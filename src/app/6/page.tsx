dbb661eeac52bc0ed91d2fac55c6c8b22baf3df7
John Pham <johnphammail@gmail.com>
2023-08-04 16:30:00 -0700

    add demo 6

---
+++ src/app/6/page.tsx
@@ -0,0 +1,108 @@
+// @ts-nocheck
+"use client";
+
+import {
+  generateRandomId,
+  getRandomAngle,
+  getRandomAnimationCurve,
+} from "@/lib/utils";
+import { useState } from "react";
+import { useInterval } from "usehooks-ts";
+import { RoomProvider, useBroadcastEvent } from "@/liveblocks.config";
+
+const REACTIONS = [
+  { label: "fire", display: "🔥" },
+  { label: "heart", display: "❤️" },
+  { label: "octopus", display: "🐙" },
+  { label: "mountain", display: "🏔️" },
+];
+
+/** In milliseconds. */
+const ANIMATION_DURATION = 2000;
+
+export default function Reactions() {
+  return (
+    <RoomProvider id="seattlejs-conf">
+      <Component />
+    </RoomProvider>
+  );
+}
+
+const Component = () => {
+  const [allReactions, setAllReactions] = useState([]);
+  const broadcast = useBroadcastEvent();
+
+  // Remove stale reactions.
+  useInterval(() => {
+    const now = new Date().getTime();
+
+    setAllReactions((reactions) =>
+      reactions.filter((reaction) => {
+        const timePassed = now - reaction.timestamp;
+
+        return timePassed < ANIMATION_DURATION;
+      })
+    );
+  }, ANIMATION_DURATION);
+
+  return (
+    <aside
+      className="flex items-center justify-center w-full h-full gap-2 text-4xl"
+      role="group"
+      aria-label="Live stream reactions"
+    >
+      {REACTIONS.map(({ display, label }) => {
+        const reactions = allReactions.filter(
+          (reaction) => reaction.label === label
+        );
+
+        return (
+          <button
+            key={label}
+            aria-label={`${label} reaction`}
+            className="relative w-10 h-10"
+            onClick={() => {
+              const newReaction = {
+                id: generateRandomId(),
+                label,
+                timestamp: new Date().getTime(),
+                curve: getRandomAnimationCurve(),
+                startingAngle: getRandomAngle(),
+              };
+
+              setAllReactions((prev) => [...prev, newReaction]);
+              broadcast({
+                type: "reaction",
+                emoji: label,
+              });
+            }}
+          >
+            <span className="relative z-10">{display}</span>
+            <div aria-live="polite" role="log" aria-atomic>
+              <div className="sr-only">
+                {reactions.length} {label} reactions
+              </div>
+              <div aria-hidden className="absolute inset-0">
+                {reactions.map((reaction) => {
+                  return (
+                    <div
+                      key={reaction.id}
+                      className={`absolute animate-reaction`}
+                      style={{
+                        // Sets transform: rotate();
+                        "--starting-angle": `${reaction.startingAngle}deg`,
+                        animationTimingFunction: reaction.curve,
+                      }}
+                    >
+                      {display}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </button>
+        );
+      })}
+    </aside>
+  );
+};
---
+++ src/liveblocks.config.ts
@@ -0,0 +1,47 @@
+import { createClient } from "@liveblocks/client";
+import { createRoomContext } from "@liveblocks/react";
+
+const client = createClient({
+  publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!,
+});
+
+type Presence = {};
+
+type Storage = {};
+
+type UserMeta = {};
+
+type RoomEvent = {
+  type: "react";
+  emoji: string;
+};
+
+export const {
+  suspense: {
+    RoomProvider,
+    useRoom,
+    useMyPresence,
+    useUpdateMyPresence,
+    useSelf,
+    useOthers,
+    useOthersMapped,
+    useOthersConnectionIds,
+    useOther,
+    useBroadcastEvent,
+    useEventListener,
+    useErrorListener,
+    useStorage,
+    useObject,
+    useMap,
+    useList,
+    useBatch,
+    useHistory,
+    useUndo,
+    useRedo,
+    useCanUndo,
+    useCanRedo,
+    useMutation,
+    useStatus,
+    useLostConnectionListener,
+  },
+} = createRoomContext<Presence, Storage, UserMeta, RoomEvent>(client);
