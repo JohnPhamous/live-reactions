f2a5eed94cef483be85bb7a2a26602e7cd7f6839
John Pham <johnphammail@gmail.com>
2023-08-04 13:58:30 -0700

    add demo 4

---
+++ src/app/4/page.tsx
@@ -0,0 +1,84 @@
+// @ts-nocheck
+"use client";
+
+import { generateRandomId } from "@/lib/utils";
+import { useState } from "react";
+import { useInterval } from "usehooks-ts";
+
+const REACTIONS = [
+  { label: "fire", display: "🔥" },
+  { label: "heart", display: "❤️" },
+  { label: "octopus", display: "🐙" },
+  { label: "mountain", display: "🏔️" },
+];
+
+/** In milliseconds. */
+const ANIMATION_DURATION = 2000;
+
+export default function Reactions() {
+  const [allReactions, setAllReactions] = useState([]);
+
+  // Remove stale reactions.
+  useInterval(() => {
+    const now = new Date().getTime();
+
+    setAllReactions((reactions) =>
+      reactions.filter((reaction) => {
+        const timePassed = now - reaction.timestamp;
+
+        return timePassed < ANIMATION_DURATION;
+      })
+    );
+  }, ANIMATION_DURATION);
+
+  return (
+    <aside
+      className="flex items-center justify-center w-full h-full gap-2 text-4xl"
+      role="group"
+      aria-label="Live stream reactions"
+    >
+      {REACTIONS.map(({ display, label }) => {
+        const reactions = allReactions.filter(
+          (reaction) => reaction.label === label
+        );
+
+        return (
+          <button
+            key={label}
+            aria-label={`${label} reaction`}
+            className="relative w-10 h-10"
+            onClick={() => {
+              setAllReactions((prev) => [
+                ...prev,
+                {
+                  id: generateRandomId(),
+                  label,
+                  timestamp: new Date().getTime(),
+                },
+              ]);
+            }}
+          >
+            <span className="relative z-10">{display}</span>
+            <div aria-live="polite" role="log" aria-atomic>
+              <div className="sr-only">
+                {reactions.length} {label} reactions
+              </div>
+              <div aria-hidden className="absolute inset-0">
+                {reactions.map((reaction) => {
+                  return (
+                    <div
+                      key={reaction.id}
+                      className="absolute animate-reaction"
+                    >
+                      {display}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </button>
+        );
+      })}
+    </aside>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        reaction: "travel 2s ease-in-out 1 forwards",
+      },
+      keyframes: {
+        travel: {
+          "0%": {
+            transform: "translate(0, -50px)",
+            rotate: "var(--starting-angle)",
+            scale: "0",
+            opacity: "0",
+          },
+          "5%": {
+            scale: "0.1",
+            opacity: "0.2",
+          },
+          "40%": {
+            opacity: "1",
+            scale: "0.3",
+          },
+          "60%": {
+            opacity: "1",
+          },
+          "100%": {
+            transform: "translate(0, -80px)",
+            scale: "1.5",
+            opacity: "0",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
+};
